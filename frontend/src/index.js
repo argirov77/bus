25cd1642ebedd9f491b3cb4f7c84f9608a2d800d
argirov77 <164051115+argirov77@users.noreply.github.com>
2025-07-25 15:33:50 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import axios from "axios";
+import { API_URL } from "./config";
+
+axios.defaults.baseURL = API_URL;
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
